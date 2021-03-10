a9e001d3325e36c92c40a985eb2c1e67888795ce
Rohit Parab <rohitparab213@gmail.com>
2021-03-10 19:35:08 +0530

    Implemented the delete functionality

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -21,6 +21,15 @@ export const createPost = (post) => async (dispatch) => {
     }
 }
 
+export const deletePost = (id) => async (dispatch) => {
+    try {
+        await api.deletePost(id)
+        dispatch({type:'DELETE',payload:id})
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
 export const updatePost = (id,post) => async (dispatch) => {
     try {
        const { data } = await api.updatePost(id,post)
