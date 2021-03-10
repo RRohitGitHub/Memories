f73a529835e52985298a8af63d7c765b7c1024a7
Rohit Parab <rohitparab213@gmail.com>
2021-03-10 14:35:53 +0530

    Corrected error with === sign in reducer and Form.js

--- client/src/components/Forms/Form.js
+++ client/src/components/Forms/Form.js
@@ -11,7 +11,7 @@ const Form =({ currentId, setCurrentId }) =>{
         creator:'',title:'',message:'',tags:'',selectedFile:''
     })
 
-    const post = useSelector((state) => currentId ? state.posts.find((p) => p._id ==currentId ) : null)
+    const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId ) : null)
   
     const dispatch = useDispatch()
     const classes =  useStyles()
